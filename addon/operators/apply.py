96083bee72507427ba764a7072048e4b059749c8
Annatroken <117992590+Annatroken@users.noreply.github.com>
2026-07-07 15:16:31 +0200

    Add ADG_OT_apply_damage operator
    Implement an operator to apply procedural damage in Blender.

--- addon/operators/apply.py
+++ addon/operators/apply.py
@@ -1,1 +1,70 @@
+from __future__ import annotations
 
+import bpy
+
+from bpy.types import Operator
+
+from ..core.engine import DamageEngine
+from ..core.logger import LOGGER
+
+
+class ADG_OT_apply_damage(Operator):
+    """Apply procedural damage"""
+
+    bl_idname = "adg.apply_damage"
+    bl_label = "Apply Damage"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+
+        obj = context.active_object
+
+        return (
+            obj is not None
+            and obj.type == "MESH"
+            and context.mode == "SCULPT"
+        )
+
+    def execute(self, context):
+
+        try:
+
+            engine = DamageEngine(context)
+
+            engine.execute()
+
+            self.report(
+                {'INFO'},
+                "Auto Damage executed."
+            )
+
+            return {'FINISHED'}
+
+        except Exception as exc:
+
+            LOGGER.exception(exc)
+
+            self.report(
+                {'ERROR'},
+                str(exc)
+            )
+
+            return {'CANCELLED'}
+
+
+classes = (
+    ADG_OT_apply_damage,
+)
+
+
+def register():
+
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
